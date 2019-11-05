8416d3d49916bb7c236d30a559bb58dae40085f4
rebeccamark <rebecca.mark@47deg.com>
2019-11-05 15:54:22 -0800

    Adds zipperComonad impl

--- src/main/scala/Comonad.scala
+++ src/main/scala/Comonad.scala
@@ -1,7 +1,8 @@
 trait Comonad[W[_]] {
   // exctracts a value from the context
   def counit[A](w: W[A]): A
-  def coflatMap[A,B](w: W[A])(f: A => W[B]): W[W[B]]
   // duplicating the effect's structure
   def cojoin[A](w: W[A]): W[W[A]]
+
+  def coflatMap[A,B](w: W[A])(f: W[A] => B): W[B]
 }
--- src/main/scala/StreamZipper.scala
+++ src/main/scala/StreamZipper.scala
@@ -14,9 +14,46 @@ case class StreamZipper[A](left: Stream[A], focus: A, right: Stream[A]) {
     val rightValues = this.right.toList.mkString(", ")
     leftValues ++ focus ++ rightValues
   }
+
+  // TODO: Generalize these
+  def streamRights: Stream[StreamZipper[A]] = Stream.iterate(this)(_.moveRight)
+    .tail
+    .zip(right)
+    .map(_._1)
+
+  def streamLefts: Stream[StreamZipper[A]] = Stream.iterate(this)(_.moveLeft)
+    .tail
+    .zip(left)
+    .map(_._1)
+
+  def streamRF[B](f:StreamZipper[A] => B): Stream[B] = Stream.iterate(this)(_.moveRight)
+    .tail
+    .zip(right)
+    .map(t => f(t._1))
+
+  def streamLF[B](f:StreamZipper[A] => B): Stream[B] = Stream.iterate(this)(_.moveLeft)
+    .tail
+    .zip(left)
+    .map(t => f(t._1))
+
+}
+
+object StreamZipperOps {
+  implicit def zipperComonad: Comonad[StreamZipper] = new Comonad[StreamZipper] {
+    override def counit[A](w: StreamZipper[A]): A = w.focus
+    override def cojoin[A](w: StreamZipper[A]): StreamZipper[StreamZipper[A]] = {
+      StreamZipper(w.streamLefts, w, w.streamRights)
+    }
+    override def coflatMap[A, B](w: StreamZipper[A])(f: StreamZipper[A] => B): StreamZipper[B] = {
+      StreamZipper(w.streamLF(f), f(w.focus), w.streamRF(f))
+    }
+  }
 }
 
 object test extends App {
+
+  import StreamZipperOps._
+
   val testZip = StreamZipper(Stream(4,3,2,1), 5, Stream(6,7,8))
   println(testZip.prettyPrint)
   println(testZip.moveLeft.prettyPrint)
