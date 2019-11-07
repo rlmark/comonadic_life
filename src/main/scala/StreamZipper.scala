aa8a39b8b4e241d12f7aaf40a8138d7f2827900c
rebeccamark <rebecca.mark@47deg.com>
2019-11-06 17:38:54 -0800

    WIP reorganizing code

--- src/main/scala/StreamZipper.scala
+++ src/main/scala/StreamZipper.scala
@@ -15,28 +15,18 @@ case class StreamZipper[A](left: Stream[A], focus: A, right: Stream[A]) {
     leftValues ++ focus ++ rightValues
   }
 
-  def streamRightF[B](f:StreamZipper[A] => B): Stream[B] = Stream.iterate(this)(_.moveRight)
+  def streamRightF[B](f:StreamZipper[A] => B): Stream[B] =
+    Stream.iterate(this)(_.moveRight)
     .tail
     .zip(right)
     .map(t => f(t._1))
 
-  def streamLeftF[B](f:StreamZipper[A] => B): Stream[B] = Stream.iterate(this)(_.moveLeft)
+  def streamLeftF[B](f:StreamZipper[A] => B): Stream[B] =
+    Stream.iterate(this)(_.moveLeft)
     .tail
     .zip(left)
     .map(t => f(t._1))
 
-}
-
-object StreamZipperOps {
-  implicit def zipperComonad: Comonad[StreamZipper] = new Comonad[StreamZipper] {
-    override def counit[A](w: StreamZipper[A]): A = w.focus
-    override def cojoin[A](w: StreamZipper[A]): StreamZipper[StreamZipper[A]] = {
-      StreamZipper(w.streamLeftF(identity), w, w.streamRightF(identity))
-    }
-    override def coflatMap[A, B](w: StreamZipper[A])(f: StreamZipper[A] => B): StreamZipper[B] = {
-      StreamZipper(w.streamLeftF(f), f(w), w.streamRightF(f))
-    }
-  }
 }
 
 object test extends App {
---
+++ src/main/scala/StreamZipperOps.scala
@@ -0,0 +1,11 @@
+object StreamZipperOps {
+  implicit def zipperComonad: Comonad[StreamZipper] = new Comonad[StreamZipper] {
+    override def counit[A](w: StreamZipper[A]): A = w.focus
+    override def cojoin[A](w: StreamZipper[A]): StreamZipper[StreamZipper[A]] = {
+      StreamZipper(w.streamLeftF(identity), w, w.streamRightF(identity))
+    }
+    override def coflatMap[A, B](w: StreamZipper[A])(f: StreamZipper[A] => B): StreamZipper[B] = {
+      StreamZipper(w.streamLeftF(f), f(w), w.streamRightF(f))
+    }
+  }
+}
