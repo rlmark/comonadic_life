25f34ac751a696e990882491d2ac1295a5f2ec12
rebeccamark <rebecca.mark@47deg.com>
2019-11-10 22:59:59 -0800

    wip storeComonadInstance

--- src/main/scala/Store.scala
+++ src/main/scala/Store.scala
@@ -2,14 +2,15 @@ case class Store[S, A](query: S => A)(val index: S)
 
 object StoreComonad {
   type StoreCoordinates[A] = Store[(Int, Int), A]
-
-  implicit def storeComonadInstance[S]: Comonad[StoreCoordinates] = {
+  implicit def storeComonadInstance: Comonad[StoreCoordinates] = {
     new Comonad[StoreCoordinates] {
       override def counit[A](w: StoreCoordinates[A]): A = w.query(w.index)
 
-      override def coFlatten[A](w: StoreCoordinates[A]): StoreCoordinates[StoreCoordinates[A]] = ???
+      override def coflatten[A](w: StoreCoordinates[A]): StoreCoordinates[StoreCoordinates[A]] = Store(Store(w.query))(w.index)
 
-      override def coflatMap[A, B](w: StoreCoordinates[A])(f: StoreCoordinates[A] => B): StoreCoordinates[B] = ???
+      // don't think this is right but...
+      override def coflatMap[A, B](w: StoreCoordinates[A])(f: StoreCoordinates[A] => B): StoreCoordinates[B] =
+        Store((_:(Int, Int)) => f(w))(w.index)
     }
   }
 }
--- src/main/scala/syntax.scala
+++ src/main/scala/syntax.scala
@@ -1,7 +1,7 @@
 object syntax {
   implicit class streamZipperSyntax[A](self: StreamZipper[A])(implicit c: Comonad[StreamZipper]) {
     def coUnit: A = c.counit(self)
-    def coflatten: StreamZipper[StreamZipper[A]] = c.coFlatten(self)
+    def coflatten: StreamZipper[StreamZipper[A]] = c.coflatten(self)
     def coflatMap[B](f: StreamZipper[A] => B): StreamZipper[B]= c.coflatMap(self)(f)
   }
 
