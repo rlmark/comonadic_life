6492884a26a19ab700021023c2e5a9e10fe59414
rebeccamark <rebecca.mark@47deg.com>
2019-11-16 14:18:14 -0800

    nelComonad syntax

--- src/main/scala/catlike/syntax/streamZipper.scala
+++ src/main/scala/catlike/syntax/streamZipper.scala
@@ -29,3 +29,16 @@ object storeComonad {
     def map[B](f: A => B): CoordinateStore[B] = c.map(self)(f)
   }
 }
+
+object nelComonad {
+  import catlike.data.Nel
+  implicit class nelComonadSyntax[A](self: Nel[A])(implicit c: Comonad[Nel]) {
+    def extract: A = c.extract(self)
+
+    def duplicate: Nel[Nel[A]] = c.duplicate(self)
+
+    def coflatMap[B](f: Nel[A] => B): Nel[B] = c.coflatMap(self)(f)
+
+    def map[B](f: A => B): Nel[B] = c.map(self)(f)
+  }
+}
