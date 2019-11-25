fc8961d4658aef3c2df4746800331bfda1532f24
rebeccamark <rebecca.mark@47deg.com>
2019-11-24 17:45:18 -0800

    WIP error handling

--- src/main/scala/conway/Main.scala
+++ src/main/scala/conway/Main.scala
@@ -2,8 +2,8 @@ package conway
 
 import catlike.data.GridZipper
 import cats.effect.{ExitCode, IO, IOApp, Sync, Timer}
+import cats.syntax.all._
 import conway.Game._
-import conway.Swarms._
 import fs2.{Stream => StreamF}
 import catlike.syntax.gridZipper._
 
@@ -22,6 +22,7 @@ object Main extends IOApp {
 
     coords.grouped(width).toList
   }
+
   def buildGrid(setCellValue: Coordinates => Int, width: Int): GridZipper[Int] = {
     val coordinates: List[List[Coordinates]] = createCoordinateLists(width)
     val gridZipperCoordinates: GridZipper[(Int, Int)] = GridZipper.fromLists(coordinates)
@@ -35,30 +36,23 @@ object Main extends IOApp {
   }
 
   def setCellValue(coord: (Int, Int), initialStateMap: Map[Coordinates, Int]): Int = {
-    val initialState: Map[(Int, Int), Int] = (glider.at(1, 1)) ++ (beacon at(12, 16)) ++ (blinker at(5, 14)) ++ (dieHard at(7, 7))
-    initialStateMap.getOrElse(coord, 0) // Setting everything not in the shapes above to be 0
+    initialStateMap.getOrElse(coord, 0)
   }
-  import cats.syntax.all._
 
   def gameLoop[F[_] : Timer : Sync]: F[StreamF[F, GridZipper[Int]]] = {
     val console = new Console[F]()
-    // 1. Ask reader what shape they want to place, and where
-    // 2. Build up a map based on that - sensitive to width of grid.
     for {
       vis <- console.getVisualization
       width <- console.getWidth
-      buildShapes <- console.loop(Map.empty)
+      userShapeInput <- console.placeUserShapes
     } yield {
       val render = new Renderer[F](vis)
-      StreamF.iterate(buildGrid(setCellValue(_ , buildShapes), width))(generation) // run subsequent generations over a seed grid
+      StreamF.iterate(
+        buildGrid(setCellValue(_ , userShapeInput), width) // build initial grid with user presents
+      )(generation) // run subsequent generations over the seed grid
         .evalTap(grid => render.renderFrame(grid)) // prints image to console
         .zipLeft(StreamF.awakeEvery[F](300.millis)) // sets the frame rate
     }
-//    val render = new Renderer[F](Visualization.Ocean) // pick a visualization
-//
-//    StreamF.iterate(buildGrid(setCellValue))(generation) // run subsequent generations over a seed grid
-//      .evalTap(grid => render.renderFrame(grid)) // prints image to console
-//      .zipLeft(StreamF.awakeEvery[F](300.millis)) // sets the frame rate
   }
 
   override def run(args: List[String]): IO[ExitCode] =
--- src/main/scala/conway/Renderer.scala
+++ src/main/scala/conway/Renderer.scala
@@ -3,44 +3,59 @@ package conway
 import catlike.data.GridZipper
 import catlike.syntax.gridZipper._
 import catlike.syntax.streamZipper._
-import cats.effect.{Sync, Timer}
+import cats.effect.Sync
 import cats.syntax.all._
 import conway.Main.Coordinates
+import Main.InitOps
 
 import scala.sys.process._
 import scala.util.Try
 
 class Console[F[_] : Sync] {
+  private def readInt: F[Int] = {
+    for {
+      s <- Sync[F].delay(scala.io.StdIn.readLine)
+      int <- Sync[F].fromTry(Try(Math.abs(s.trim.toInt))).handleErrorWith(_ =>
+        Sync[F].delay(println("Input should be an Integer"))
+          .flatMap(_ => readInt)
+      )
+    } yield int
+  }
+
   def getVisualization: F[Visualization] = {
     for {
-      _ <- Sync[F].delay(println("Pick a visualization style:"))
+      _ <- Sync[F].delay(println("Pick a visualization style"))
       _ <- Sync[F].delay(println("Enter 1 for Ocean, 2 for Forest"))
-      s <- Sync[F].delay(scala.io.StdIn.readLine)
-    } yield (
-      if (s == "1") Visualization.Ocean else Visualization.Forest
-      )
+      int <- readInt
+      vis <- {
+        val v: F[Visualization] = {
+          if (int == 1) Sync[F].delay(Visualization.Ocean)
+          else if (int == 2) Sync[F].delay(Visualization.Forest)
+          else getVisualization
+        }
+        v
+      }
+    } yield vis
   }
 
   def getWidth: F[Int] = {
     for {
-      _ <- Sync[F].delay(println("How wide do you want your board to be"))
+      _ <- Sync[F].delay(println("How wide do you want your board to be?"))
       _ <- Sync[F].delay(println("Max is 100, min is 3"))
-      s <- Sync[F].delay(scala.io.StdIn.readLine)
-      int <- Sync[F].fromTry(Try(s.toInt))
+      int <- readInt
       sanitizedInt <- {
         val greaterThan3 = math.max(int, 3)
         val lessThan100 = math.min(greaterThan3, 100)
         Sync[F].delay(lessThan100)
       }
-    } yield (sanitizedInt)
+    } yield sanitizedInt
   }
 
   def getUserShape: F[Map[Coordinates, Int]] = {
     for {
       _ <- Sync[F].delay(println("Select a shape"))
-      _ <- Sync[F].delay(println("Enter 1 for Blinker, 2 for Glider"))
-      s <- Sync[F].delay(scala.io.StdIn.readLine)
-      int <- Sync[F].fromTry(Try(s.toInt))
+      _ <- Sync[F].delay(println("Enter 1 for Blinker, 2 for Glider")) // TODO, full set of shapes
+      int <- readInt
     } yield (
       if (int == 1) Swarms.blinker
       else if (int == 2) Swarms.glider
@@ -55,54 +70,57 @@ class Console[F[_] : Sync] {
       s <- Sync[F].delay(scala.io.StdIn.readLine)
       coordinates <- Sync[F].fromTry {
         Try {
-          val intList: List[Int] = s.split(",").toList.map(_.toInt)
+          val intList: List[Int] = s.split(",").toList.map(_.trim.toInt)
           val x :: y :: Nil = intList
           (x, y)
         }
-      }
+      }.handleErrorWith(_ => getCoordinates)
     } yield (coordinates)
   }
 
-  import Main.InitOps
-  def setShapeAtCoordinates(): F[Map[Coordinates, Int]] = {
+  def setShapeAtCoordinates: F[Map[Coordinates, Int]] = {
     for {
       userShape <- getUserShape
       coordinates <- getCoordinates
     } yield userShape.at(coordinates)
   }
 
-  def loop(acc : Map[Coordinates, Int]): F[Map[Coordinates, Int]] = {
-    for {
-      map <- setShapeAtCoordinates()
-      _ <- Sync[F].delay(println( "Press 0 when done setting board, press any other Int to continue"))
-      s <- Sync[F].delay(scala.io.StdIn.readLine)
-      map2 <- if(s.toInt == 0) Sync[F].delay(acc ++ map) else loop(acc ++ map)
-    } yield(map2)
+  def placeUserShapes: F[Map[Coordinates, Int]] = {
+    def loop(acc: Map[Coordinates, Int]): F[Map[Coordinates, Int]] = {
+      for {
+        currentShape <- setShapeAtCoordinates
+        _ <- Sync[F].delay(println("Press 0 when done setting board, press any other Int to continue"))
+        int <- readInt
+        coordinateMap <- if (int == 0) Sync[F].delay(acc ++ currentShape) else loop(acc ++ currentShape)
+      } yield (coordinateMap)
+    }
+    loop(Map.empty)
   }
 }
-  class Renderer[F[_] : Timer : Sync](visualization: Visualization) {
 
-    def clear: F[Int] = Sync[F].delay("clear".!)
+class Renderer[F[_] : Sync](visualization: Visualization) {
 
-    def cellRepresentation(value: Int): String = {
-      val alive = visualization.alive
-      val background = visualization.background
-      if (value == 1) alive else background
-    }
+  def clear: F[Int] = Sync[F].delay("clear".!)
 
-    def format(grid: GridZipper[Int]): String = {
-      grid.map(i => cellRepresentation(i))
-        .value
-        .map(_.toList)
-        .toList
-        .map(_.mkString)
-        .mkString("\n")
-    }
-
-    def renderFrame(gridZipper: GridZipper[Int]): F[Unit] = {
-      for {
-        _ <- clear
-        _ <- Sync[F].delay(println(format(gridZipper)))
-      } yield ()
-    }
+  def cellRepresentation(value: Int): String = {
+    val alive = visualization.alive
+    val background = visualization.background
+    if (value == 1) alive else background
   }
+
+  def format(grid: GridZipper[Int]): String = {
+    grid.map(i => cellRepresentation(i))
+      .value
+      .map(_.toList)
+      .toList
+      .map(_.mkString)
+      .mkString("\n")
+  }
+
+  def renderFrame(gridZipper: GridZipper[Int]): F[Unit] = {
+    for {
+      _ <- clear
+      _ <- Sync[F].delay(println(format(gridZipper)))
+    } yield ()
+  }
+}
